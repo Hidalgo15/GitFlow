ae05c8c9215e6b8c6c988f6432864741c09fec0e
Hidalgo15 <emmanuelperezramirez15@gmail.com>
2025-11-24 21:22:42 -0400

    Electronico

---
+++ GitFlow Tarea 3/GitFlow Tarea 3/Base Entity/Producto.cs
@@ -0,0 +1,23 @@
+﻿
+namespace GitFlow_Tarea_3.Base_Entity
+{
+    public abstract class Producto
+    {
+        public int Id { get; protected set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public  abstract string Tipo { get; } // Propiedad abstracta
+
+        protected Producto(int id, string nombre, decimal precio)
+        {
+            Id = id;
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Tipo: {Tipo}, Nombre: {Nombre}, Precio: {Precio:C}";
+        }
+    }
+}
